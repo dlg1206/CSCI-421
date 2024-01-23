4b6b4b1a03d9177582c5ba30c51a6839b06c8975
Derek Garcia <dlg1206@g.rit.edu>
2024-01-23 12:38:49 -0500

    feat(cli): init InsertInto command
    Refs: #1

---
+++ cli/cmd/commands/InsertInto.java
@@ -0,0 +1,28 @@
+package cli.cmd.commands;
+
+import cli.cmd.execption.ExecutionFailure;
+import cli.cmd.execption.InvalidUsage;
+
+/**
+ * <b>File:</b> InsertInto.java
+ * <p>
+ * <b>Description: Command to insert data into a table in the database<</b>
+ *
+ * @author Derek Garcia
+ */
+public class InsertInto extends Command {
+
+    public InsertInto(String args) throws InvalidUsage {
+        // TODO
+    }
+
+    @Override
+    protected void helpMessage() {
+        // TODO
+    }
+
+    @Override
+    public void execute() throws ExecutionFailure {
+        // TODO
+    }
+}
