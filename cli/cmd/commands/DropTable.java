3ff468aabb06b03f0e1b77946900ecc52a7bd9ae
Derek Garcia <dlg1206@g.rit.edu>
2024-01-23 12:38:34 -0500

    feat(cli): init Display command
    Refs: #1

---
+++ cli/cmd/commands/DropTable.java
@@ -0,0 +1,28 @@
+package cli.cmd.commands;
+
+import cli.cmd.execption.ExecutionFailure;
+import cli.cmd.execption.InvalidUsage;
+
+/**
+ * <b>File:</b> DropTable.java
+ * <p>
+ * <b>Description: Command to create a new table in the database</b>
+ *
+ * @author Derek Garcia
+ */
+public class DropTable extends Command {
+
+    public DropTable(String args) throws InvalidUsage {
+        // TODO
+    }
+
+    @Override
+    protected void helpMessage() {
+        // TODO
+    }
+
+    @Override
+    public void execute() throws ExecutionFailure {
+        // TODO
+    }
+}
