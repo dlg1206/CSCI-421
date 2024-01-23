ddd19643ef8b24203ae8c3deb90f7b192541e483
Derek Garcia <dlg1206@g.rit.edu>
2024-01-23 12:38:10 -0500

    feat(cli): init AlterTable command
    Refs: #1

---
+++ cli/cmd/commands/AlterTable.java
@@ -0,0 +1,27 @@
+package cli.cmd.commands;
+
+import cli.cmd.execption.ExecutionFailure;
+import cli.cmd.execption.InvalidUsage;
+
+/**
+ * <b>File:</b> AlterTable.java
+ * <p>
+ * <b>Description: Command to alter a table in the database</b>
+ *
+ * @author Derek Garcia
+ */
+public class AlterTable extends Command {
+    public AlterTable(String args) throws InvalidUsage {
+        // TODO
+    }
+
+    @Override
+    protected void helpMessage() {
+        // TODO
+    }
+
+    @Override
+    public void execute() throws ExecutionFailure {
+        // TODO
+    }
+}
