f2bd9f88182aaa14657fc34e3e53f7d72b63721d
Derek Garcia <dlg1206@g.rit.edu>
2024-01-23 12:39:04 -0500

    feat(cli): init Select command
    Refs: #1

---
+++ cli/cmd/commands/Select.java
@@ -0,0 +1,28 @@
+package cli.cmd.commands;
+
+import cli.cmd.execption.ExecutionFailure;
+import cli.cmd.execption.InvalidUsage;
+
+/**
+ * <b>File:</b> Select.java
+ * <p>
+ * <b>Description: Command to select items from the database</b>
+ *
+ * @author Derek Garcia
+ */
+public class Select extends Command {
+
+    public Select(String args) throws InvalidUsage {
+        // TODO
+    }
+
+    @Override
+    protected void helpMessage() {
+        // TODO
+    }
+
+    @Override
+    public void execute() throws ExecutionFailure {
+        // TODO
+    }
+}
