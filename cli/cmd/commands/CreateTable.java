67aa522b43e440c30536eb9c15dc44250643c0a0
Derek Garcia <dlg1206@g.rit.edu>
2024-01-23 12:31:21 -0500

    feat(cli): init create table command
    Refs: #1

---
+++ cli/cmd/commands/CreateTable.java
@@ -0,0 +1,28 @@
+package cli.cmd.commands;
+
+import cli.cmd.execption.ExecutionFailure;
+import cli.cmd.execption.InvalidUsage;
+
+/**
+ * <b>File:</b> CreateTable.java
+ * <p>
+ * <b>Description: Command to create a new table in the database</b>
+ *
+ * @author Derek Garcia
+ */
+public class CreateTable extends Command{
+
+    public CreateTable(String args) throws InvalidUsage{
+        // TODO
+    }
+
+    @Override
+    protected void helpMessage() {
+        // TODO
+    }
+
+    @Override
+    public void execute() throws ExecutionFailure {
+        // TODO
+    }
+}
