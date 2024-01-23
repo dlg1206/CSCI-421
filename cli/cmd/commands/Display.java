c310e5294ea5231728369e27f15595c613a1cba6
Derek Garcia <dlg1206@g.rit.edu>
2024-01-23 12:38:25 -0500

    feat(cli): init Display command
    Refs: #1

---
+++ cli/cmd/commands/Display.java
@@ -0,0 +1,28 @@
+package cli.cmd.commands;
+
+import cli.cmd.execption.ExecutionFailure;
+import cli.cmd.execption.InvalidUsage;
+
+/**
+ * <b>File:</b> Display.java
+ * <p>
+ * <b>Description: Command to display information about the database</b>
+ *
+ * @author Derek Garcia
+ */
+public class Display extends Command {
+
+    public Display(String args) throws InvalidUsage {
+        // TODO
+    }
+
+    @Override
+    protected void helpMessage() {
+        // TODO
+    }
+
+    @Override
+    public void execute() throws ExecutionFailure {
+        // TODO
+    }
+}
