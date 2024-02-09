d420405ca5137c0f7155049d49570a15eae04db1
Derek Garcia <dlg1206@g.rit.edu>
2024-02-09 14:28:52 -0500

    feat(PageBuffer): stub out page buffer methods
    Refs: #14, #4

--- src/sm/StorageManager.java
+++ src/sm/StorageManager.java
@@ -2,6 +2,8 @@ package sm;
 
 import sm.tmp.DataType;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -14,12 +16,86 @@ import java.util.List;
 public class StorageManager {
 
 
+    private class PageBuffer{
+
+        private record Page(int tableID, int number, Byte[] data){}
+
+        private final List<Page> buffer;
+        private final int capacity;
+        private final int pageSize;
+
+        public PageBuffer(int bufferSize, int pageSize){
+            this.buffer = Arrays.asList(new Page[bufferSize]);
+            this.capacity = bufferSize;
+            this.pageSize = pageSize;
+        }
+
+        private void readToBuffer(int tableID, int pageNum){
+            /*
+            // Make room if needed
+            if this.buffer.size() == this.capacity
+                this.buffer.remove( this.capacity - 1 );
+
+            // Read new page into buffer. Add to first b/c assume going to be accessed
+            int offset = pageCount + pageSize * pageNum;
+            this.buffer.add( new Page(
+                tableID,
+                pageNum,
+                sys.loadBytes(tableID) + offset;  // ie load 1 page however it's done
+                )
+            );
+             */
+        }
+
+        private Page readFromBuffer(int tableID, int pageNum){
+            /*
+            Page page = null;     // assume not in buf
+            // Check buffer for page
+            for p : this.buffer
+                if p.tableId == tableID && p.number == pageNumber
+                    page = p
+                    break
+
+             // Update access if in buffer
+             if page != null
+                this.buffer.remove(page)
+                this.buffer.add(page)
+
+             return page;
+             */
+            return null;
+        }
+
+        private boolean isPageInBuffer(int tableID, int pageNum){
+            /*
+            for p : this.buffer
+                if p.tableId == tableID && p.number == pageNumber
+                    return true
+             */
+            return false;
+        }
+
+
+        public Byte[] getPage(int tableID, int pageNum){
+            /*
+            if( !isPageInBuffer(tableID, pageNum) )
+                readToBuffer(tableID, pageNum);
+
+            return readFromBuffer(tableID, pageNum).data;
+
+             */
+            return null;
+        }
+
+    }
+
+
 
      private int getPageCount(int tableID){
          return 1;
      }
 
-     private byte[] getPage(int tableID, int pageNum){
+     private Byte[] getPage(int tableID, int pageNum){
          return null;
      }
 
