ea8b39e2c2ba21405985295082a779a5a048875a
Derek Garcia <dlg1206@g.rit.edu>
2024-02-09 15:00:34 -0500

    feat(PageBuffer): stub out byte utilities to page
    Refs: #14, #4

--- src/sm/StorageManager.java
+++ src/sm/StorageManager.java
@@ -17,7 +17,17 @@ public class StorageManager {
 
     private class PageBuffer{
 
-        private record Page(int tableID, int number, Byte[] data){}
+        private record Page(int tableID, int number, Byte[] data){
+            public int getNumberOfRecords(){
+                // parse data byte array to get number of records
+                return -1;
+            }
+
+            public Byte[] getRecordBytes(){
+                // truncate # records from data byte array
+                return null;
+            }
+        }
 
         private final List<Page> buffer;
         private final int capacity;
@@ -29,11 +39,20 @@ public class StorageManager {
             this.pageSize = pageSize;
         }
 
+        private void writeToDisk(int tableID, Page page){
+            // write to disk
+        }
+
         private void readToBuffer(int tableID, int pageNum){
             /*
             // Make room if needed
-            if this.buffer.size() == this.capacity
-                this.buffer.remove( this.capacity - 1 );
+            if this.buffer.size() == this.capacity{
+                writeToDisk(
+                    tableID,
+                    this.buffer.remove( this.capacity - 1 )
+                );
+            }
+
 
             // Read new page into buffer. Add to first b/c assume going to be accessed
             int offset = pageCount + pageSize * pageNum;
@@ -85,16 +104,17 @@ public class StorageManager {
             return readFromBuffer(tableID, pageNum).data;
         }
 
+
     }
 
 
     private final PageBuffer buffer;
-    private final String databasePath;
+    private static String databasePath;
 
 
     public StorageManager(int bufferSize, int pageSize, String databasePath){
         this.buffer = new PageBuffer(bufferSize, pageSize);
-        this.databasePath = databasePath;
+        StorageManager.databasePath = databasePath;
     }
 
     private int getPageCount(int tableID){
