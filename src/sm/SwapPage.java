1f8c710544e30fd77f07e5c3406159b178607540
Derek Garcia <dlg1206@g.rit.edu>
2024-02-22 17:57:17 -0500

    docs: update SwapPage docstrings
    Refs: #3

--- src/sm/SwapPage.java
+++ src/sm/SwapPage.java
@@ -3,20 +3,25 @@ package sm;
 /**
  * <b>File:</b> SwapPage.java
  * <p>
- * <b>Description:</b>
+ * <b>Description:</b> Utility class to help distinguish between swap and regular pages
  *
  * @author Derek Garcia
  */
-public class SwapPage extends Page{
+public class SwapPage extends Page {
     /**
-     * Create new Empty Page
+     * Create new Swap Page
      *
-     * @param writeFile
+     * @param writeFile  File to write page to
      * @param pageSize   Max number of records page can hold
      * @param pageNumber Page Number
-     * @param data
+     * @param data       Page byte data
      */
     public SwapPage(TableFile writeFile, int pageSize, int pageNumber, byte[] data) {
         super(writeFile, pageSize, pageNumber, data);
+    }
+
+    @Override
+    public String toString() {
+        return "SWAP: " + this.getWriteFile().getTableID() + ": " + getPageNumber();
     }
 }
