5b2d9c942138b50905baaa8cd4b8e8be0fea92a2
Clinten Hopkins <70732411+DevWalrus@users.noreply.github.com>
2024-02-23 15:22:56 -0500

    Fix insert into table's regex

--- src/cli/cmd/commands/InsertInto.java
+++ src/cli/cmd/commands/InsertInto.java
@@ -39,7 +39,7 @@ public class InsertInto extends Command {
     private static final String NO_QUOTES_MSG = "The attribute '%s' takes a string, which must be wrapped in quotes. You did not do this for tuple #%s";
 
     private static final Pattern FULL_PATTERN = Pattern.compile("insert[\\s\\t]+into[\\s\\t]+([a-z0-9]*)[\\s\\t]+values[\\s\\t]+((?:\\([0-9\\s\"a-z.]+\\),*[\\s\\t]*)+);", Pattern.CASE_INSENSITIVE);
-    private static final Pattern TABLE_NAME_PATTERN = Pattern.compile("[a-z][a-z0-9]", Pattern.CASE_INSENSITIVE);
+    private static final Pattern TABLE_NAME_PATTERN = Pattern.compile("[a-z][a-z0-9]*", Pattern.CASE_INSENSITIVE);
     private static final Pattern TUPLE_PATTERN = Pattern.compile("\\(\\s*([0-9\\s\"a-zA-Z.]+)\\s*\\)", Pattern.CASE_INSENSITIVE);
     private static final Pattern STRING_PATTERN = Pattern.compile("\"(.+)\"", Pattern.CASE_INSENSITIVE);
 
